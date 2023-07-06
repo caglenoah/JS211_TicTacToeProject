9f978b0e885404869de8da889e2ab03b3e235d8c
caglenoah <caglenoah@gmail.com>
2023-07-06 15:00:11 -0600

    added check for wins, confused on the assertions

--- main.js
+++ main.js
@@ -32,25 +32,64 @@ const printBoard = () => {
   console.log('2 ' + board[2].join(' | '));
 }
 
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
 const horizontalWin = () => {
   // Your code here to check for horizontal wins
+  if ((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X")
+    || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") 
+  ) 
+{
+  return true
+  }
+  else if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") 
+  || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")
+  )
+    { return true }
+  else if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") 
+  || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")
+  ) 
+  { return true
+    }
 }
 
 const verticalWin = () => {
   // Your code here to check for vertical wins
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") 
+|| (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")
+  ) {
+    return true
+}
+else if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") 
+|| (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")
+) { return true}
+else if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") 
+|| (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")
+  ) {
+    return true
+}
+
 }
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") 
+|| (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")
+  ) {
+    return true
+}
+else if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") 
+|| (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")
+  ) {
+    return true
 }
 
-const checkForWin = () => {
-  // Your code here call each of the check for types of wins
-}
-
-const ticTacToe = (row, column) => {
-  // Your code here to place a marker on the board
-  // then check for a win
 }
 
 const getPrompt = () => {
